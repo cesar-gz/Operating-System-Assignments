d4d9589c6715e946715ddd18ab23dbbcf50dbefb
cesar-gz <cesargutierrezceja@gmail.com>
2022-10-05 19:20:07 -0700

    I think I made progress?

--- cs351_assignment3_Cesar_Gutierrez.cpp
+++ cs351_assignment3_Cesar_Gutierrez.cpp
@@ -1,19 +1,25 @@
 #include <iostream>
 #include <fstream>
-#include <string>
 #include <Windows.h>
 using namespace std;
 
 // ConsoleApplication1.cpp : Simple Command Line Interpreter.
-// Copyright (c) Cesar Gutierrez 9/29/2022
+// Copyright (c) Cesar Gutierrez 10/05/2022
 //
 
+HANDLE myMutex;
+HANDLE handleThreadA;
+HANDLE handleThreadB;
+DWORD threadA;
+DWORD threadB;
+
 void print26times(char A, int X){
-    // open file
+
     ofstream file("synch.txt");
 
-    if(A == 'A'){
+    WaitForSingleObject(myMutex, INFINITE);
 
+    if(A == 'A'){
         // print this line 20 times
         for(int i = 0; i < 20; i++){
             // print this array
@@ -25,10 +31,6 @@ void print26times(char A, int X){
             file << "\n";
             A = 'A';
         }
-
-        // close file
-        file.close();
-        return;
     }
     else if(X == 1){
         // print this line 20 times
@@ -42,37 +44,61 @@ void print26times(char A, int X){
             file << "\n";
             X = 1;
         }
-
-        // close file
-        file.close();
-        return;
     }
+
+    ReleaseMutex(myMutex);
+
+    file.close();
 }
 
 DWORD WINAPI ThreadA(){
-    print26times('A', 0);
+    char letter = 'Z';
+    int number = 1;
+    print26times(letter, number);
+    return 0;
+}
+
+DWORD WINAPI ThreadB(){
+    char letter2 = 'A';
+    int number2 = 0;
+    print26times(letter2, number2);
     return 0;
 }
 
 int main(){
 
-    // create and close a file called synch.txt
     ofstream file("synch.txt");
     file.close();
 
-    // Create thread A
-    HANDLE handleThread;
-                DWORD thread;
-                handleThread = CreateThread(
-                    NULL,                            // default security attribute
-                    0,                               // default 1MB size
-                    (LPTHREAD_START_ROUTINE)ThreadA, // function address to be called
-                    0,                               // function parameter to pass, takes pointer
-                    0,                               // creation flag
-                    &thread                          // address of thread
-                );
-                WaitForSingleObject(handleThread, INFINITE);
-                CloseHandle(handleThread);
+    myMutex = CreateMutex(
+        NULL,   // no security attributes
+        FALSE,  // mutex owner is no one right now
+        NULL    //no name
+    );
+
+    handleThreadA = CreateThread(
+        NULL,                            // default security attribute
+        0,                               // default 1MB size
+        (LPTHREAD_START_ROUTINE)ThreadA, // function address to be called
+        0,                               // function parameter to pass, takes pointer
+        0,                               // creation flag
+        &threadA                         // address of thread
+    );
+
+    handleThreadB = CreateThread(
+        NULL,                            // default security attribute
+        0,                               // default 1MB size
+        (LPTHREAD_START_ROUTINE)ThreadB, // function address to be called
+        0,                               // function parameter to pass, takes pointer
+        0,                               // creation flag
+        &threadB                         // address of thread
+    );
+
+    WaitForSingleObject(handleThreadA, INFINITE);
+    WaitForSingleObject(handleThreadB, INFINITE);
+
+    CloseHandle(handleThreadA);
+    CloseHandle(handleThreadB);
 
     return 0;
 }
